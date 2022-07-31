a315af6923e4ddebb64fee6f75c6a3d8a0b2f3d2
Gebbi01 <106231783+Gebbi01@users.noreply.github.com>
2022-07-31 17:32:35 +0700

    Upload Portfolio Code

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Settingan buat manggil file dari direktori lain
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + ('public/img')))
+
+// Settingan views
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('server berhasil berjalan di port 3000!')
+})
